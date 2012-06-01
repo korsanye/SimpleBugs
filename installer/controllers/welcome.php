b022152f22fa067d59cadf91ba8f4acf098b7dc7
Repox <storm@catalystcode.net>
2012-06-01 12:56:48 +0200

    Updated installer and removed unnessecay methods

--- installer/controllers/welcome.php
+++ installer/controllers/welcome.php
@@ -81,39 +81,6 @@ class Welcome extends CI_Controller {
 		}
 	}
 	
-	public function user()
-	{
-
-		$this->form_validation->set_error_delimiters('<p class="error">', '</p>');
-		$this->form_validation->set_rules('email', 'lang:email', 'trim|required|valid_email');
-		$this->form_validation->set_rules('password', 'lang:password', 'required');
-		$this->form_validation->set_rules('password_confirm', 'lang:password_confirm', 'required|matches[password]');		
-
-		if ($this->form_validation->run() == TRUE)
-		{ 			
-			$this->load->database();
-			$this->config->set_item('encryption_key', 'tmp');
-			
-			if( ! $this->simpleusers->create('Admin', $this->input->post('password'), $this->input->post('email'), TRUE ) )
-			{
-				show_error( lang('couldnt_create_user'), lang('couldnt_create_user_message') );
-				
-			}
-			else
-			{
-				redirect('setup/done');
-				exit;
-			}
-		}		
-		
-		$this->load->view('setup/create_user');
-	}
-	
-	public function done()
-	{
-		$this->load->view('setup/done');
-	}
-	
 	private function _get_installation_files()
 	{
 		$files[] = FCPATH."application/config/config.php";
@@ -132,7 +99,7 @@ class Welcome extends CI_Controller {
 		$search = array('{hostname}', '{username}', '{password}', '{database}', '{dbdriver}', '{dbprefix}');
 		$replace = array(set_value('db_hostname'), set_value('db_username'), set_value('db_password'), set_value('db_database'), set_value('db_driver'), set_value('db_prefix'));
 		$database = str_replace($search, $replace, $database);		
-		file_put_contents(FCPATH."application/config/database.php", $config);
+		file_put_contents(FCPATH."application/config/database.php", $database);
 	}
 	
 
@@ -149,17 +116,29 @@ class Welcome extends CI_Controller {
 	public function db_connection_test( $str )
 	{
 		unset($this->db);
+
+		$db['default']['hostname'] = 'localhost';
+		$db['default']['username'] = 'root';
+		$db['default']['password'] = 'yfvHX47M';
+		$db['default']['database'] = 'thebugs';
+		$db['default']['dbdriver'] = 'mysql';
+		$db['default']['dbprefix'] = 'sb_';
 				
 		$db_config['hostname'] = set_value('db_hostname');
 		$db_config['username'] = set_value('db_username');
-		$db_config['password'] = set_value('db_password');
-		$db_config['database'] = set_value('db_database');
+		$db_config['password'] = set_value('db_password');		
 		$db_config['dbprefix'] = set_value('db_prefix');
 		$db_config['dbdriver'] = set_value('db_driver');
-		
-		
-		$db_config['db_debug'] = false;
-		
+		$db_config['pconnect'] = FALSE;
+		$db_config['db_debug'] = FALSE;
+		$db_config['cache_on'] = FALSE;
+		$db_config['cachedir'] = '';
+		$db_config['char_set'] = 'utf8';
+		$db_config['dbcollat'] = 'utf8_general_ci';
+		$db_config['swap_pre'] = '';
+		$db_config['autoinit'] = TRUE;
+		$db_config['stricton'] = FALSE;
+					
 		$this->load->database($db_config);
 		
 	
@@ -167,15 +146,17 @@ class Welcome extends CI_Controller {
 		{
 			$this->load->dbutil();
 	
-			if ($this->dbutil->database_exists($this->db->database))
+			if ($this->dbutil->database_exists(set_value('db_database')))
 			{
+				$this->db->query('USE '.set_value('db_database'));
 				return TRUE;
 			}
 			else
 			{
 				$this->load->dbforge();
-				if ($this->dbforge->create_database($this->db->database))
+				if ($this->dbforge->create_database(set_value('db_database')))
 				{
+					$this->db->query('USE '.set_value('db_database'));
 					return TRUE;
 				}
 				else
@@ -194,4 +175,3 @@ class Welcome extends CI_Controller {
 }
 
 /* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
